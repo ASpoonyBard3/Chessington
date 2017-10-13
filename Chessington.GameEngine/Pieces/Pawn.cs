a64ef3232f099aa516e4212d685403bfbe2e10b5
ASpoonyBard3 <samsonf21@gmail.com>
2017-10-13 12:32:36 +0100

    Completed first unit test for pawn pieces, added log for basic pawn moving up and down the chess board.

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -5,12 +5,24 @@ namespace Chessington.GameEngine.Pieces
 {
     public class Pawn : Piece
     {
-        public Pawn(Player player) 
+        public Pawn(Player player)
             : base(player) { }
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var position = board.FindPiece(this);
+            var availableMoves = new List<Square>();
+            if (Player == Player.Black)
+            {
+                availableMoves.Add(Square.At(position.Row + 1, position.Col));
+            }
+            else
+            {
+                availableMoves.Add(Square.At(position.Row - 1, position.Col));
+            }
+
+            return availableMoves;
         }
+
     }
 }
